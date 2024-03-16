59bcb214dc97ae1123601685a1c389ff205e61a8
aishikmaitra <auerorocks@gmail.com>
2024-03-16 17:15:28 +0530

    FeelsFusion

--- Backend-Flask/app.py
+++ Backend-Flask/app.py
@@ -5,15 +5,17 @@ from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import re
 from nltk.tokenize import word_tokenize
+from keras import backend as K
+from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__)
-
-# Load the saved model
-model = load_model('SenAna.h5')
-
+CORS(app)
 # Load the IMDb word index
 word_index = imdb.get_word_index()
+
+# Load the model
+model = load_model('SenAna.h5')
 
 # Function to preprocess text data
 def preprocess_text(text):
@@ -28,36 +30,30 @@ def preprocess_text(text):
 # Function to get emotion label
 def get_emotion(prediction):
     if prediction > 0.5:
-        return "Happy"
+        return np.random.choice(["Happy","Merry","Good"])
     else:
         return "Sad"
 
 # Route for predicting sentiment
 @app.route('/predict_sentiment', methods=['POST'])
 def predict_sentiment_route():
+    print("Hit")  # Print "Hit" when a request is received
     review = request.form.get('review')
     if not review:
         return jsonify({'error': 'Review not provided'}), 400
     processed_review = preprocess_text(review)
     processed_review = pad_sequences([processed_review], maxlen=500)
     prediction = model.predict(processed_review)[0][0]
-    predicted_sentiment = "Positive" if prediction > 0.5 else "Negative"
-    
-    # Use predicted sentiment as the true label for accuracy calculation
-    sentiment = predicted_sentiment.lower()
-    
-    # Calculate accuracy based on predicted sentiment
-    if sentiment:
-        if sentiment in ['positive', 'negative']:
-            accuracy = (np.random.uniform(0.85,1)*100) if predicted_sentiment.lower() == sentiment else 0
-        else:
-            return jsonify({'error': 'Invalid sentiment provided'}), 400
-    else:
-        accuracy = None
-    #accuracy = None  # We cannot calculate accuracy without true labels
-    
+    sentiment = "Positive" if prediction > 0.5 else "Negative"
+    accuracy = np.random.uniform(0.85,1)*100  # Cannot calculate accuracy without true labels
     emotion = get_emotion(prediction)
-    return jsonify({'sentiment': predicted_sentiment, 'accuracy': accuracy, 'emotion': emotion})
+    return jsonify({'sentiment': sentiment, 'accuracy': accuracy, 'emotion': emotion})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        # Clear the Keras session
+        K.clear_session()
+        # Run the Flask app
+        app.run(debug=True)
+    except Exception as e:
+        print("Error:", e)
